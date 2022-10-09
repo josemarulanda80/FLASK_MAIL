7ec9191cc9e01685b045ae165e3fc4b17dc7f867
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-09 12:51:37 -0500

    genial

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+mail= Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'josemarulanda64@gmail.com'
+app.config['MAIL_PASSWORD'] = 'wthhdemuicchihpz'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index():
+   msg = Message('Hello', sender = 'josemarulanda64@gmail.com', recipients = ['josemarulanda64@gmail.com'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
+
+if __name__ == '__main__':
+   app.run(debug = True)
